b7229270bd88abdb761e4cbfe4e6f740d5aa7eb7
feronat <c14180099@john.petra.ac.id>
2021-05-20 10:48:13 +0700

    funemyclass

---
+++ src/app/globalservice.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GlobalserviceService {
+
+  constructor() { }
+
+  // public getboolean(){
+  //   return this.btnid;
+  // }
+  // public setboolean(btnid:boolean){
+  //   this.btnid=btnid;
+  // }
+}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Attribute, Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +7,65 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  show: boolean;
+  ceksatu: boolean;
+  btnid: number = 0;
+  constructor() { }
+
+  ngOnInit() {
+    // this.show= this.globalvar.getboolean();
+  }
+
+  clickedfav(event) {
+
+    if (this.show == true) {
+      this.btnid = event.srcElement.id;
+      this.show = false;
+      // alert(this.btnid);
+      // alert(this.show);
+      if (this.btnid == 1) {
+        this.ceksatu = false;
+
+      }
+    }
+    else {
+      this.btnid = event.srcElement.id;
+      this.show = true;
+      // alert(this.btnid);
+      // alert(this.show);  //pertamakali
+      if (this.btnid == 1) {
+        this.ceksatu = true;
+      }
+    }
+  }
+
+  // clickedfav2(event){
+
+  //   if(this.show==true){
+  //     this.show=false;
+  //     console.log(event.srcElement.id);
+  //     console.log(event.srcElement.name);
+  //     alert(event.srcElement.id);
+  //     alert(event.srcElement.name);
+  //   }
+  //   else{
+  //     this.show=true;
+  //     console.log(event.srcElement.id);
+  //     console.log(event.srcElement.name);
+  //     alert(event.srcElement.id);
+  //     alert(event.srcElement.name);
+
+  //   }
+  // }
+
+  //   clickedfav(ionicButton, btn: any) {
+  //     if(this.answer == btn){
+  //       ionicButton.color =  'success';
+  //     } else {
+  //       ionicButton.color =  'danger';
+  //     }
+  // }
+
+
 
 }
